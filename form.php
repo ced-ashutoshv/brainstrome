351dd4beb9571a778e779e19df80dafafdcca91f
Ashutosh Verma <ashutoshverma@cedcoss.com>
2021-12-01 11:16:16 +0530

    template added

---
+++ form.php
@@ -0,0 +1,83 @@
+<form action="action.php" method="post">
+    <body>
+        <div class="card">
+        <h2>Contact Us</h2>
+        <div class="row">
+            <div class="col">
+                <div class="form-group">
+                    <label>Name</label>
+                    <input type="text" name="stu_name">
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="form-group">
+                    <label>Reg No</label>
+                    <input type="text" name="stu_name">
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="form-group">
+                    <label>Course</label>
+                    <input type="text" name="stu_name">
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="form-group">
+                    <label>Course Code</label>
+                    <input type="text" name="stu_name">
+                </div>
+            </div>
+            
+            <div class="col">
+                <div class="form-group">
+                    <label>Reg No</label>
+                    <input type="text" name="stu_name">
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="form-group">
+                    <label>CAT1</label>
+                    <input type="number" name="cat1_name">
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="form-group">
+                    <label>CAT2</label>
+                    <input type="number" name="cat2_name">
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="form-group">
+                    <label>DA2</label>
+                    <input type="number" name="da2_name">
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="form-group">
+                    <label>DA1</label>
+                    <input type="number" name="da1_name">
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="form-group">
+                    <label>QUIZ</label>
+                    <input type="number" name="quiz_name">
+                </div>
+            </div>
+
+            Name, , , , Marks for 5 Subjects (CAT1, CAT2, DA1, DA2 and QUIZ)
+            <div class="col">
+                <input type="submit" value="Submit">
+            </div>
+        </div>
+        </div>
+    </body>
+</form>
